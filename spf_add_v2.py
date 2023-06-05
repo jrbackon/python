cf17c00e65f0df93f7db0009f8b9e354f5b995a8
Jacob Backon <jbackon@babson.edu>
2023-06-05 12:00:35 -0400

    Added several functions.

--- spf_add_v2.py
+++ spf_add_v2.py
@@ -1,3 +1,6 @@
+import ipaddress
+import math
+
 # load the existing record
 def load_record():
     record = input("What is the path of the existing spf record? ")
@@ -32,25 +35,99 @@ def load_additions():
     return new_list
 # create a list of all IPs
 def combine_lists(spf_list, additions):
-    for ip in additions():
+    for ip in additions:
         spf_list.append(ip)
     return spf_list
     
-    # create a list of all networks
-# combine the two lists
+# create a list of all networks
+def network_list(spf_list):
+    networks = []
+    for address in spf_list:
+        if "/" in address:
+            networks.append(address)
+    return networks
+
 # convert networks into IP addresses
 # convert the IP strings to IP4 or IP6 objects
 # sort the list
+def address_convert(spf_list):
+    converted_list = []
+    for address in spf_list:
+        if "/" in address:
+            converted_list.append(ipaddress.ip_address(address.split("/", 1)[0]))
+        else:
+            converted_list.append(ipaddress.ip_address(address))
+    return sorted(converted_list)
+
 # convert the list back into a list of string objects
+def sorted_strings(converted_list):
+    string_list = []
+    for address in converted_list:
+        string_list.append(str(address))
+    return string_list
+
 # compare the list of string addresses to the list of networks
-    # if there is a match we replace the string address with the network
+# if there is a match we replace the string address with the network
 # add ip4: to the beginning of each item in the list
-# calculate total number of sublists len(list) / 29
-# add 29 addresses to each sublist
+def network_replace(string_list, network_list):
+    formatted_list = []
+    for address in string_list:
+        for network in network_list:
+            if network.split("/", 1)[0] == address:
+                formatted_list.append("ip4:"+network)
+            else:
+                pass
+        formatted_list.append("ip4:"+address)
+    return formatted_list
+
+
+# add 26 addresses to each sublist
+def add_toRecords(formatted_list, sublists):
+    record_dict = {}
+    location = 0
+    for i in range(sublists):
+        count = 0
+        record_dict[i] = []
+        for address in formatted_list:
+            try:
+                if count <= 26:
+                    record_dict[i].append(formatted_list[location + count])
+                    count += 1
+                else:
+                    location = location + count
+                    break
+            except IndexError:
+                break
+    return record_dict
+
 # add the spf info to each sublist
+def add_recordInfo(record_dict):
+    for i in range(len(record_dict)):
+        if i == 0:
+            record_dict[i].insert(0, "babson.edu")
+            record_dict[i].insert(1, "v=spf1")
+            record_dict[i].append("include :spf"+str(i+1)+".babson.edu ~all")
+        elif i == len(record_dict):
+            record_dict[i].insert(0, "spf"+str(i)+".babson.edu")
+            record_dict[i].insert(1, "v=spf1")
+            record_dict[i].append("~all")
+        else:
+            record_dict[i].insert(0, "spf"+str(i)+".babson.edu")
+            record_dict[i].insert(1, "v=spf1")
+            record_dict[i].append("include:spf"+str(i+1)+".babson.edu ~all")
+    return record_dict
+
 # convert each sublist to a string
     # write each list to a file
 
 # print(create_spfList(load_record()))
 #print(load_additions())
-print(combine_lists(create_spfList(load_record()), load_additions()))
+combined_record = combine_lists(create_spfList(load_record()), load_additions())
+network_list = network_list(combined_record)
+sorted_list = sorted_strings(address_convert(combined_record))
+# print(network_replace(sorted_list, network_list))
+
+# calculate total number of sublists len(list) / 29
+sublists = math.ceil(len(network_replace(sorted_list, network_list))/29)
+record_dict = add_toRecords(network_replace(sorted_list, network_list), sublists)
+print(add_recordInfo(record_dict))
